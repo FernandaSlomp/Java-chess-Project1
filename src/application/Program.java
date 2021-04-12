4807e0b9eb95667b0c8d145f490f7b0b508df557
Fernanda Slomp <fernandaslomp@hotmail.com>
2021-04-12 15:52:46 -0300

    CHECKMATE!

--- src/application/Program.java
+++ src/application/Program.java
@@ -23,7 +23,7 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		List<ChessPiece> captured = new ArrayList<>();
 		
-		while(true) {
+		while(!chessMatch.getCheckMate()) {
 			try {
 				UI.clearScreen();
 				UI.printMatch(chessMatch, captured);
@@ -51,6 +51,9 @@ public class Program {
 				sc.nextLine();
 			}
 		}
+		
+		UI.clearScreen();
+		UI.printMatch(chessMatch, captured);
 
 	}
 
